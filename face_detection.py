017998dabd87e577540968ffacd78be27e66b17a
kimyeonsooo <kimyeon@gmail.com>
2023-06-05 16:38:32 +0900

    Updated face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,13 +1,6 @@
 import cv2
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-age = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
+age_list = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
             '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-gender = ['Male', 'Female']
-
-def highlight(net, frame, conf_threshold=0.7):
-    frame_opencv_dnn = frame.copy()
-
-def process(face_net, age_net, gender_net, frame, padding=20):
-    result_img, face_boxes = highlight(face_net, frame)
-    data = []
+gender_list = ['Male', 'Female']
