a2ce9034a0b858a771651d74ab562e84de15b3db
kimyeonsooo <kimyeon@gmail.com>
2023-06-10 23:43:05 +0900

    process_frame: if, for

--- face_detection.py
+++ face_detection.py
@@ -30,3 +30,12 @@ def highlight_face(net, frame, conf_threshold=0.7):
 def process_frame(face_net, age_net, gender_net, frame, padding=20):
     result_img, face_boxes = highlight_face(face_net, frame)
     data = []
+
+    if not face_boxes:
+        print("No face detected")
+
+    for face_box in face_boxes:
+        face = frame[max(0, face_box[1]-padding):
+                     min(face_box[3]+padding, frame.shape[0]-1),
+                     max(0, face_box[0]-padding):
+                     min(face_box[2]+padding, frame.shape[1]-1)]
